c62526f58708ccd4d2e46400afb2ef0eb20141af
Argonislav <gregro.03@spst.eu>
2022-03-03 13:16:59 +0100

    Přidejte soubory projektu.

---
+++ Metody09-14GitHub/Metody.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody09_14GitHub
+{
+    class Metody
+    {
+        public static bool Kvadratickarovnice(out double diskriminant, out double koren1, out double koren2, double a, double b, double c)
+        {
+            diskriminant = b * b - 4 * (a * c);
+            if (diskriminant > 0)
+            {
+                koren1 = -b + Math.Sqrt(diskriminant) / 2 * a;
+                koren2 = -b - Math.Sqrt(diskriminant) / 2 * a;
+            }
+            else if (diskriminant < 0)
+            {
+                koren1 = -b + Math.Sqrt(diskriminant) / 2 * a;
+                koren2 = -b - Math.Sqrt(diskriminant) / 2 * a;
+            }
+            else
+            {
+                koren1 = -b + Math.Sqrt(diskriminant) / 2 * a;
+                koren2 = -b - Math.Sqrt(diskriminant) / 2 * a;
+            }
+            
+            bool maReseniVR = false;
+            return maReseniVR;
+        }
+    }
+}
